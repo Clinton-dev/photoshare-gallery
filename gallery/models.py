d159255c97fe48a849d5836961c8146506e1f417
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-28 21:16:41 +0300

    Add photo model

--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,5 @@
 from operator import mod
+from unicodedata import category
 from django.db import models
 
 class Category(models.Model):
@@ -12,3 +13,13 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+class Photo(models.Model):
+    image = models.ImageField(null=False, blank=False)
+    description = models.CharField(max_length=180, null=False)
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL)
+    location = models.ForeignKey(Location,on_delete=models.SET_NULL)
+
+
+    def __str__(self):
+        return self.description
