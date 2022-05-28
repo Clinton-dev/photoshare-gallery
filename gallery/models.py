24f397147989083cac02f9f17a132e7ae2b1373c
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-28 21:05:42 +0300

    Add location model

--- gallery/models.py
+++ gallery/models.py
@@ -1,8 +1,14 @@
 from operator import mod
 from django.db import models
 
-class category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=150, null=False, blank=False)
 
     def __str__(self):
         return self.name
