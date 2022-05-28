1e66c49f49fd05ae43fd6a8587e9dd4720779559
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-28 21:04:41 +0300

    Add category model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,8 @@
+from operator import mod
 from django.db import models
 
-# Create your models here.
+class category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
