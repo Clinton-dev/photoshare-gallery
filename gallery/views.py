b002c1f09018e73d5e0ea34bf3707fc4943f9c69
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-29 12:08:22 +0300

    Add dynamic list of location and category in sidebar

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,21 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Category, Photo, Location
+
+def get_category():
+    categories = Category.objects.all()
+    return categories
+
+def get_location():
+    location = Location.objects.all()
+    return location
 
 def home(request):
-    return render(request,'gallery/home.html')
+    context = {
+        'locations': get_location,
+        'categories': get_category
+    }
+    return render(request,'gallery/home.html', context)
 
 def viewPhoto(request, pk):
     return render(request,'gallery/photo.html')
