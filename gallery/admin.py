08589d74cb834b805e135d9e6818686d07a584cf
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-29 11:55:40 +0300

    Add media root and static root to settings.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Location, Photo
+admin.site.register(Photo)
+admin.site.register(Category)
+admin.site.register(Location)
