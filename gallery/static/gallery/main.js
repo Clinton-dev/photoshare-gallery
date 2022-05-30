abeeab3515b1a1b6a6c5947df4504f98b3e930bc
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-30 12:15:45 +0300

    Fix copy button bug

--- gallery/static/gallery/main.js
+++ gallery/static/gallery/main.js
@@ -1,19 +1,15 @@
-console.log('hello world');
-
 const copyBtns = [...document.getElementsByClassName('copy-btn')];
-console.log(copyBtns);
 
 let previous = null;
 
 copyBtns.forEach(btn => btn.addEventListener('click', ()=> {
-    console.log('clicked');
     const photoUrl = btn.getAttribute('data-url');
-    console.log(photoUrl);
-    navigator.clipboard.writeText(photoUrl);
+    fullUrl = 'https://galleryshare.herokuapp.com' + photoUrl
+    navigator.clipboard.writeText(fullUrl);
     btn.textContent='Copied!';
 
     if (previous) {
-        btn.textContent='Copy image url'
+         previous.textContent='Copy image url'
     }
     previous = btn
 }))
