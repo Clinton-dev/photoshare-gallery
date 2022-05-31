05dd680f7a694672d62a525e933d2b3cc5965fbb
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-31 14:30:06 +0300

    Add for category, location and photo

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Category, Photo, Location
+
+class PhotoTest(TestCase):
+    def testfields(self):
+        photo = Photo()
+        photo.description = 'foo bar'
+        photo.image = 'foobar.jpg'
+        photo.save()
+
+        record = Photo.objects.get(pk=1)
+        self.assertAlmostEquals(record, photo)
+
+class CategoryTest(TestCase):
+    def testfields(self):
+        category = Category()
+        category.name = 'foo bar'
+        category.save()
+
+        record = Category.objects.get(pk=1)
+        self.assertAlmostEquals(record, category)
+
+class LocationTest(TestCase):
+    def testfields(self):
+        location = Location()
+        location.name = 'foo bar'
+        location.save()
+
+        record = Location.objects.get(pk=1)
+        self.assertAlmostEquals(record, location)
